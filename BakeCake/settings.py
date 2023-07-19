54cdd36278232164461d91b6d9d71e7eacd46322
Anna Veselova <anna.veselova.97@mail.ru>
2023-07-19 21:28:23 +0300

    rename folder datacenter to bot

--- BakeCake/settings.py
+++ BakeCake/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'datacenter.apps.DatacenterConfig',
+    'bot.apps.botConfig',
 ]
 
 MIDDLEWARE = [
---
+++ bot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class botConfig(AppConfig):
+    name = 'bot'
