4660ee447196ae7e14e0c457f88aad44418647dd
Anna Veselova <anna.veselova.97@mail.ru>
2023-07-19 03:13:07 +0300

    add Client model

--- datacenter/models.py
+++ datacenter/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CakeParam(models.Model):
     title = models.CharField('Название', max_length=20)
     price = models.DecimalField(
-        'Цена', 
+        'Цена',
         default=0.00,
         max_digits=6, decimal_places=2)
     is_available = models.BooleanField('Есть в наличии', default=False)
@@ -22,7 +22,7 @@ class Levels(CakeParam):
     title = models.IntegerField(
         'Количество уровней',
         choices=LEVEL_CHOICES,
-        unique=True, 
+        unique=True,
         default=1)
 
 
@@ -50,41 +50,48 @@ class Cake(models.Model):
 
     levels = models.ForeignKey(
         Levels,
-        verbose_name='Уровни', 
+        verbose_name='Уровни',
         default=1,
         on_delete=models.CASCADE)  # какой on_delete лучше?
     shape = models.ForeignKey(
         Shape,
-        verbose_name='Форма', 
+        verbose_name='Форма',
         on_delete=models.CASCADE)  # какой on_delete лучше?
     topping = models.ForeignKey(
         Topping,
-        verbose_name='Топпинг', 
+        verbose_name='Топпинг',
         on_delete=models.CASCADE)  # какой on_delete лучше?
     berries = models.ForeignKey(
         Berries,
         verbose_name='Ягоды',
-        null=True, blank=True, 
+        null=True, blank=True,
         on_delete=models.SET_NULL)  # какой on_delete лучше?
     topping = models.ForeignKey(
         Topping,
         verbose_name='Топпинг',
-        null=True, blank=True, 
+        null=True, blank=True,
         on_delete=models.SET_NULL)  # какой on_delete лучше?
     text = models.CharField(
-        'Надпись на торте', 
-        max_length=100, 
+        'Надпись на торте',
+        max_length=100,
         null=True, blank=True)
 
     def __str__(self):
         return self.title   # если нет названия, f(){описание из компонентов}
 
     def get_price(self):
-        cake_params = [self.levels, self.shape, self.topping, self.berries, self.topping]
+        cake_params = [self.levels, self.shape, 
+                       self.topping, self.berries, self.topping]
         return sum([param.price for param in cake_params])
 
 
-
+class Client(models.Model):
+    id_telegram = models.CharField('Телеграм id', max_length=20)
+    name = models.CharField('Имя', max_length=30, default='Дорогой Гость')
+    address = models.CharField('Адрес', max_length=80, null=True, blank=True)
+    consent_to_pdProc = models.BooleanField(
+        'Согласие на обработку ПД',
+        default=False)
 
 
 
