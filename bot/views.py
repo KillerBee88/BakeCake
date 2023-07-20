07bb58aa313ea13d4d9c050278eb5cc91c44873e
Anna Veselova <anna.veselova.97@mail.ru>
2023-07-20 08:39:31 +0300

    add verify_cake()

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from datacenter.models import Cake
 
-# Create your views here.
+
+def verify_cake(cake_id):
+	cake = get_object_or_404(Cake, id=int(cake_id))
+	for param in cake.get_params():
+		if not param.is_available:
+			return False
+	return True
+
