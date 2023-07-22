edcfb75b8abbbd0b8bff4e8ead355f225fa1e954
KillerBee88 <hardmen08@gmail.com>
2023-07-22 20:44:34 +0300

    Update settings.py

--- BakeCake/settings.py
+++ BakeCake/settings.py
@@ -118,7 +118,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LANGUAGES = [
+    ('ru', ('Russian')),
+]
 
 TIME_ZONE = 'Europe/Moscow'
 
